4a89b0194d97a832e1871aa98bff8d99dc0f0125
Dymion Fritz <DympyDev@gmail.com>
2025-04-20 00:53:46 +0200

    feat(dceu): added DCEU (only movies)

--- src/lib/constants/collections.constants.ts
+++ src/lib/constants/collections.constants.ts
@@ -1,6 +1,7 @@
+import type { MediaCollection } from '$lib/types/media.types';
 import { MCU_PHASES } from './mcu.constants';
 import { STAR_WARS_SECTIONS } from './star-wars.constants';
-import type { MediaCollection } from '$lib/types/media.types';
+import { DCEU_MOVIES } from './dceu-movies.constants';
 
 export const COLLECTIONS: MediaCollection[] = [
     {
@@ -9,6 +10,13 @@ export const COLLECTIONS: MediaCollection[] = [
         description: 'Follow the epic saga of the Marvel Cinematic Universe from Iron Man to the latest releases. Track your progress through all the movies and Disney+ shows.',
         coverImage: '/images/mcu/banner.webp',
         sections: MCU_PHASES
+    },
+    {
+        key: 'dceu',
+        name: 'DC Extended Universe',
+        description: 'Follow the epic saga of the DC Extended Universe from Man of Steel to the latest releases. Track your progress through all the movies and Disney+ shows.',
+        coverImage: '/images/dceu/banner.webp',
+        sections: DCEU_MOVIES
     },
     {
         key: 'star-wars',
---
+++ src/lib/constants/dceu-movies.constants.ts
@@ -0,0 +1,107 @@
+import type { MediaSection } from "../types/media.types";
+
+export const DCEU_MOVIES: MediaSection[] = [
+  {
+    name: "DC Extended Universe Movies",
+    media: [
+        {
+            id: 'wonder-woman',
+            name: 'Wonder Woman',
+            image: '/images/dceu/wonder_woman.avif',
+            watched: false,
+            comicSize: 'half'
+        },
+        {
+            id: 'wonder-woman-1984',
+            name: 'Wonder Woman 1984',
+            image: '/images/dceu/wonder_woman_84.avif',
+            watched: false,
+            comicSize: 'half'
+        },
+        {
+            id: 'man-of-steel',
+            name: 'Man of Steel',
+            image: '/images/dceu/man_of_steel.avif',
+            watched: false,
+            comicSize: 'full'
+        },
+        {
+            id: 'batman-v-superman',
+            name: 'Batman v Superman: Dawn of Justice',
+            image: '/images/dceu/batman_v_superman.avif',
+            watched: false,
+            comicSize: 'two-thirds'
+        },
+        {
+            id: 'suicide-squad',
+            name: 'Suicide Squad',
+            image: '/images/dceu/suicide_squad.jpg',
+            watched: false,
+            comicSize: 'third'
+        },
+        {
+            id: 'justice-league',
+            name: 'Justice League',
+            image: '/images/dceu/justice_league.webp',
+            watched: false,
+            comicSize: 'full'
+        },
+        {
+            id: 'shazam',
+            name: 'Shazam!',
+            image: '/images/dceu/shazam.avif',
+            watched: false,
+            comicSize: 'half'
+        },
+        {
+            id: 'birds-of-prey',
+            name: 'Birds of Prey: And the Fantabulous Emancipation of One Harley Quinn',
+            image: '/images/dceu/birds_of_prey.avif',
+            watched: false,
+            comicSize: 'half'
+        },
+        {
+            id: 'suicide-squad-2',
+            name: 'The Suicide Squad',
+            image: '/images/dceu/the_suicide_squad.avif',
+            watched: false,
+            comicSize: 'two-thirds'
+        },
+        {
+            id: 'black-adam',
+            name: 'Black Adam',
+            image: '/images/dceu/black_adam.webp',
+            watched: false,
+            comicSize: 'third'
+        },
+        {
+            id: 'shazam-fury-of-the-gods',
+            name: 'Shazam! Fury of the Gods',
+            image: '/images/dceu/shazam_fury_of_the_gods.avif',
+            watched: false,
+            comicSize: 'full'
+        },
+        {
+            id: 'the-flash',
+            name: 'The Flash',
+            image: '/images/dceu/the_flash.avif',
+            watched: false,
+            comicSize: 'half'
+        },
+        {
+            id: 'blue-beetle',
+            name: 'Blue Beetle',
+            image: '/images/dceu/blue_beetle.avif',
+            watched: false,
+            comicSize: 'half'
+        },
+        {
+            id: 'aqua-man-the-lost-kingdom',
+            name: 'Aquaman and the Lost Kingdom',
+            image: '/images/dceu/aquaman_lost_kingdom.webp',
+            watched: false,
+            comicSize: 'full'
+        }
+    ]
+  }
+];
